8a1b6a63b79a7941b0f62368cf8fa71dceb292fe
meiyazhakan <meiyazhakan86@gmail.com>
2024-10-22 10:45:29 +0530

    Update index.tsx

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -94,6 +94,70 @@ const Brands = () => {
         </div>
       </section>
 
+
+
+
+      <section className="py-11 mt-4 bg-[#1F0A3C] mb-15">
+        <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+          <h3 className="mx-auto mb-4 text-xl text-gray-300 text-center">
+            We are experts in
+          </h3>
+          <h4 className="mx-auto mb-4 text-3xl text-center text-white font-bold dark:text-white">
+            Microsoft Dynamics & it's services
+          </h4>
+
+          <div className="flex grid grid-cols-3 justify-center gap-7.5 md:grid-cols-3 lg:grid-cols-4 xl:gap-10">
+
+            <div className="row-span-4 col-span-2 bg-white rounded-lg border-[#1F0A3C]">
+              <img className="ml-25 mt-14 mr-2 mb-2" src="/images/blog/blog-01.png" alt="" />
+              <h5 className="ml-7 mb-3 mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Microsoft dynamics 365 F&O:Comprehensive Finacial Management</h5>
+              <p className="ml-7 mb-4 font-normal text-gray-500 dark:text-gray-200">
+                Optimze budgeting, project management and finacial operations with Microsoft Dynamics 365 Finance & Operations,tailored for large international companies.
+              </p>
+              <p>
+                <a className="ml-7 mb-4 font-normal underline text-gray-500 dark:text-gray-200">Know More</a>
+              </p>
+              <p className="mb-1"></p>
+
+            </div>
+
+            <div className="row-span-2 col-span-2 bg-white rounded-lg border-[#1F0A3C]">
+              <a href="#" className="flex flex-col items-center md:flex-row md:max-w-xl">
+                <img alt="title" width="36" height="36" src="/images/icon/icon-01.svg" className="bg-primary ml-5 mr-2" />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Microsoft dynamics 365 F&O:Comprehensive Finacial Management</h5>
+                </div>
+              </a>
+              <p className="ml-7 mb-3 font-normal text-gray-500 dark:text-gray-200">
+                Optimze budgeting, project management and finacial operations with Microsoft Dynamics 365 Finance & Operations,tailored for large international companies.
+              </p>
+              <p>
+                <a className="ml-7 mb-1 font-normal underline text-gray-500 dark:text-gray-200">Know More</a>
+              </p>
+
+            </div>
+            <div className="row-span-2 col-span-2 bg-white rounded-lg border-[#1F0A3C]">
+              <a href="#" className="flex flex-col items-center md:flex-row md:max-w-xl">
+                <img alt="title" width="36" height="36" src="/images/icon/icon-01.svg" className="bg-primary ml-5 mr-2" />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Microsoft dynamics 365 F&O:Comprehensive Finacial Management</h5>
+                </div>
+              </a>
+              <p className="ml-7 mb-3 font-normal text-gray-500 dark:text-gray-200">
+                Optimze budgeting, project management and finacial operations with Microsoft Dynamics 365 Finance & Operations,tailored for large international companies.
+              </p>
+              <p>
+                <a className="ml-7 mb-1 font-normal underline text-gray-500 dark:text-gray-200">Know More</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
       {/* <!-- ===== Clients End ===== --> */}
     </>
   );
