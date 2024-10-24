68e6892e1463065df10ae55365478ee1628c72f6
meiyazhakan <meiyazhakan86@gmail.com>
2024-10-24 10:27:54 +0530

    Update faqData.tsx

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -3,17 +3,17 @@ import { FAQ } from "@/types/faq";
 const faqData: FAQ[] = [
   {
     id: 1,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
+    quest: "Porta in ullamcorpor lorem.sed Nullam Lorem tincidut urna",
     ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
   },
   {
     id: 2,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
+    quest: "Porta in ullamcorpor lorem.sed Nullam Lorem tincidut urna",
     ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
   },
   {
     id: 3,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
+    quest: "Porta in ullamcorpor lorem.sed Nullam Lorem tincidut urna",
     ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
   },
 ];
