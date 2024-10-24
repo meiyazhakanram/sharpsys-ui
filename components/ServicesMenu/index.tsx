9a1a6726baa1fd93f5416b46e2ab5ea755e11558
meiyazhakan <meiyazhakan86@gmail.com>
2024-10-24 10:28:54 +0530

    Update index.tsx

--- components/ServicesMenu/index.tsx
+++ components/ServicesMenu/index.tsx
@@ -12,15 +12,15 @@ import StaffAugmentation from '../StaffAugmentation';
 
 
 // export default function ServicesMenu() {
-    const ServicesMenu = (props) => {
-        console.log(props);
-  const [navigationOpen, setNavigationOpen] = useState(false);
-  const [dropdownToggler, setDropdownToggler] = useState(false);
-  const [stickyMenu, setStickyMenu] = useState(false);
+const ServicesMenu = (props) => {
+    console.log(props);
+    const [navigationOpen, setNavigationOpen] = useState(false);
+    const [dropdownToggler, setDropdownToggler] = useState(false);
+    const [stickyMenu, setStickyMenu] = useState(false);
 
 
-  const pathUrl = usePathname();
-  const [isHovered, setIsHovered] = useState(0);
+    const pathUrl = usePathname();
+    const [isHovered, setIsHovered] = useState(0);
 
     const handleMouseOver = (e) => {
         switch (e.target.getAttribute('data-key')) {
@@ -38,40 +38,39 @@ import StaffAugmentation from '../StaffAugmentation';
                 break
             case "4":
                 setIsHovered(5)
-            break
+                break
         }
-      }
+    }
 
     return (
         <>
-        <header><h3 className='text-metatitle2 pl-2 text-black dark:text-white font-bold'>Our Services</h3></header>
-<div className='flex flex-col md:flex-row flex-1 gap-8'>
-            {/* Nav Menu Start   */}
-            <aside className='w-full md:w-60 bg-slate-100'>
-        {/* <div className='mb-4 inline-block bg-zumthor px-4.5 py-1.5 dark:border dark:border-strokedark dark:bg-blacksection'>Enterprise Solution</div> */}
-        <ul className='bg-grey'>
-        {serviceMenuData.map((menuItem, key) => (
-          <li key={key} data-key={key} className={`px-4.5 py-4 cursor-pointer dark:border dark:border-strokedark dark:bg-blacksection hover:bg-white rounded-lg`} onMouseOver={handleMouseOver} onMouseOut={() => setIsHovered(isHovered)}>
-            {menuItem.title}              
-</li>
+            <header><h3 className='text-metatitle2 pl-2 text-black dark:text-white font-bold'>Our Services</h3></header>
+            <div className='flex flex-col md:flex-row flex-1 gap-8'>
+                {/* Nav Menu Start   */}
+                <aside className='w-full md:w-60 bg-slate-100'>
+                    {/* <div className='mb-4 inline-block bg-zumthor px-4.5 py-1.5 dark:border dark:border-strokedark dark:bg-blacksection'>Enterprise Solution</div> */}
+                    <ul className='bg-grey'>
+                        {serviceMenuData.map((menuItem, key) => (
+                            <li key={key} data-key={key} className={`px-4.5 py-4 cursor-pointer dark:border dark:border-strokedark dark:bg-blacksection hover:bg-white rounded-lg`} onMouseOver={handleMouseOver} onMouseOut={() => setIsHovered(isHovered)}>
+                                {menuItem.title}
+                            </li>
 
-        ))}
-        </ul>
-        </aside>
-        <main className='flex-1 bg-white rounded-lg'>
-            checkDropDown();
-        {/* {(isHovered === 0 && <Enterprisesolution/> || */}
-        {(isHovered === 1 && <Enterprisesolution/> || 
-        isHovered === 2 && <CRMSolution/> ||
-        isHovered === 3 && <AdvancedAnalytics/> ||
-        isHovered === 4 && <BusinessIntelligence/> ||
-        isHovered === 5 && <StaffAugmentation/>
-        )}
-        {/* <AdvancedAnalytics/> */}
-      
-          
-        </main>
-        </div>
+                        ))}
+                    </ul>
+                </aside>
+                <main className='flex-1 bg-white rounded-lg'>
+                    {/* {(isHovered === 0 && <Enterprisesolution/> || */}
+                    {(isHovered === 1 && <Enterprisesolution /> ||
+                        isHovered === 2 && <CRMSolution /> ||
+                        isHovered === 3 && <AdvancedAnalytics /> ||
+                        isHovered === 4 && <BusinessIntelligence /> ||
+                        isHovered === 5 && <StaffAugmentation />
+                    )}
+                    {/* <AdvancedAnalytics/> */}
+
+
+                </main>
+            </div>
         </>
     )
 }
