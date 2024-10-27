1edf7f3762911f4dc880105c45dda2fb248bc917
rejeshkr <rejeshkr@virtusa.com>
2024-10-28 02:45:11 +0530

    Home page service

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -1,37 +1,11 @@
 "use client";
 import React from "react";
 import { useState } from 'react';
+import { servicesDataMap } from "./serviceDataMap";
 
-const servicesMap = {
-    "Enterprise Solution": [
-        { id: 101, order:"01", name: "Microsoft Dynamics 365 Finance & Operations", img: "/images/erp.png" },
-        { id: 102, order:"02", name: "Microsoft Dynamics 365 Business Central", img: "/images/bpm.png" },
-    ],
-    "CRM": [
-        { id: 201, order:"01", name: "Microsoft Dynamics Customer Engagement", img: "/images/erp.png" },
-        { id: 202, order:"02", name: "Salesforce", img: "/images/bpm.png" },
-    ],
-    "Advanced Analytics": [
-        { id: 301, order:"01", name: "Consulting", description: "Expert guidance to drive strategic business growth", img: "/images/consulting.png" },
-        { id: 302, order:"02", name: "Data Analytics", img: "/images/data-analytics.png" },
-        { id: 303, order:"03", name: "ETL Mitigation", img: "/images/etl.png" },
-        { id: 304, order:"04", name: "Data Migration", img: "/images/migration.png" },
-        { id: 305, order:"05", name: "Data Warehousing", img: "/images/warehousing.png" },
-        { id: 306, order:"06", name: "Machine Learning", img: "/images/ml.png" },
-    ],
-    "Business Intelligence": [
-        { id: 401, order:"01", name: "Power BI", img: "/images/erp.png" },
-        { id: 402, order:"02", name: "Tableau", img: "/images/bpm.png" },
-    ],
-    "Staff Augmentation": [
-        { id: 501, order:"01", name: "Resource Capabilities", img: "/images/erp.png" },
-        { id: 502, order:"02", name: "Industry Verticals", img: "/images/bpm.png" },
-    ],
-
-};
 
 const Services = () => {
-    const [activeTab, setActiveTab] = useState("Advanced Analytics");
+    const [activeTab, setActiveTab] = useState("Enterprise Solution");
     const [selectedItem, setSelectedItem] = useState(null);
 
     return (
@@ -45,7 +19,7 @@ const Services = () => {
                 </h4>
                 {/* Navbar */}
                 <nav className="flex flex-wrap bg-green-50 rounded space-x-4 mb-8 justify-center">
-                    {Object.keys(servicesMap).map((tab) => (
+                    {Object.keys(servicesDataMap).map((tab) => (
                         <button
                             key={tab}
                             className={`px-4 py-2 rounded ${activeTab === tab ? "bg-green-700 text-white transition rounded" : "bg-green-50"}`}
@@ -57,7 +31,7 @@ const Services = () => {
                 {/* Content */}
                 <div className="flex flex-col md:flex-row gap-8 ">
                     <div className="flex-1 border-t-2">
-                        {servicesMap[activeTab].map((item) => (
+                        {servicesDataMap[activeTab].map((item) => (
                             <div
                                 key={item.id}
                                 className={`p-3 w-96 border-b cursor-pointer hover:bg-gray-200 text-left`}
---
+++ components/Services/serviceDataMap.tsx
@@ -0,0 +1,27 @@
+export const servicesDataMap = {
+    "Enterprise Solution": [
+        { id: 101, order:"01", name: "Microsoft Dynamics 365 Finance & Operations", img: "/images/erp.png" },
+        { id: 102, order:"02", name: "Microsoft Dynamics 365 Business Central", img: "/images/bpm.png" },
+    ],
+    "CRM": [
+        { id: 201, order:"01", name: "Microsoft Dynamics Customer Engagement", img: "/images/erp.png" },
+        { id: 202, order:"02", name: "Salesforce", img: "/images/bpm.png" },
+    ],
+    "Advanced Analytics": [
+        { id: 301, order:"01", name: "Consulting", description: "Expert guidance to drive strategic business growth", img: "/images/consulting.png" },
+        { id: 302, order:"02", name: "Data Analytics", img: "/images/data-analytics.png" },
+        { id: 303, order:"03", name: "ETL Mitigation", img: "/images/etl.png" },
+        { id: 304, order:"04", name: "Data Migration", img: "/images/migration.png" },
+        { id: 305, order:"05", name: "Data Warehousing", img: "/images/warehousing.png" },
+        { id: 306, order:"06", name: "Machine Learning", img: "/images/ml.png" },
+    ],
+    "Business Intelligence": [
+        { id: 401, order:"01", name: "Power BI", img: "/images/erp.png" },
+        { id: 402, order:"02", name: "Tableau", img: "/images/bpm.png" },
+    ],
+    "Staff Augmentation": [
+        { id: 501, order:"01", name: "Resource Capabilities", img: "/images/erp.png" },
+        { id: 502, order:"02", name: "Industry Verticals", img: "/images/bpm.png" },
+    ],
+
+};
