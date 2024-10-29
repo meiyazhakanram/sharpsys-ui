723468eded924d18b2f82dde69e6776289620a39
meiyazhakan <meiyazhakan86@gmail.com>
2024-10-29 10:19:36 +0530

    Add files via upload

---
+++ app/(site)/careers/careerMapData.tsx
@@ -0,0 +1,50 @@
+import { Career } from "@/types/careers";
+
+const CareerData: Career[] = [
+    {
+        title: "Customer Success",
+        heading: {
+            positiontitle: "Position",
+            locationtitle: "Locations",
+            noofopeningstitle: "No.Of.Openings",
+            jobtypetitle: "Type"
+        },
+        job: {
+            position: "Customer onboarding manager",
+            location: "Chennai",
+            noofopenings: "02",
+            jobtype: "Full Time"
+        }
+    },
+    {
+        title: "",
+        heading: {
+            positiontitle: "Position",
+            locationtitle: "Locations",
+            noofopeningstitle: "No.Of.Openings",
+            jobtypetitle: "Type"
+        },
+        job: {
+            position: "Customer onboarding manager",
+            location: "Chennai",
+            noofopenings: "02",
+            jobtype: "Full Time"
+        }
+    },
+    {
+        title: "Design",
+        heading: {
+            positiontitle: "Position",
+            locationtitle: "Locations",
+            noofopeningstitle: "No.Of.Openings",
+            jobtypetitle: "Type"
+        },
+        job: {
+            position: "Customer onboarding manager",
+            location: "Chennai",
+            noofopenings: "02",
+            jobtype: "Full Time"
+        }
+    }
+];
+export default CareerData;
