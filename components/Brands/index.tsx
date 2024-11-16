4ef9dfb0d152b0e64f1f9ff6feb21160a5040dbf
meiyazhakan <meiyazhakan86@gmail.com>
2024-11-16 12:32:26 +0530

    Update index.tsx

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -6,11 +6,10 @@ import brandData from "./brandData";
 const Brands = () => {
   return (
     <>
-      {/* <!-- ===== Clients Start ===== --> */}
       <section className="bg-white py-11 mb-8 mt-8">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-          <h4 className="mx-auto mb-4 text-3xl text-center font-bold text-black dark:text-white">
-            Our <span className="textStartGradient">Clie</span><span className="textLastGradient">nts</span>
+          <h4 className="mx-auto mb-5 text-3xl text-center font-bold text-black dark:text-white">
+            Our Esteemed <span className="textStartGradient">Partners</span><span className="textLastGradient"> and Stakeholders</span>
           </h4>
           <div className="grid grid-cols-3 items-center justify-center gap-7.5 md:grid-cols-6 lg:gap-12.5 xl:gap-29">
             {brandData.map((brand, key) => (
