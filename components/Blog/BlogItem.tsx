19ec6117c72af6ac220bdd49b2e0fad90e5f2143
meiyazhakan <meiyazhakan86@gmail.com>
2024-11-19 10:24:28 +0530

    Update BlogItem.tsx

--- components/Blog/BlogItem.tsx
+++ components/Blog/BlogItem.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import parse from "html-react-parser";
 
 const BlogItem = ({ blog }: { blog: Blog }) => {
-  const {_id,  mainImage, title, metadata } = blog;
+  const { _id, mainImage, title, metadata } = blog;
 
   return (
     <>
@@ -32,17 +32,17 @@ const BlogItem = ({ blog }: { blog: Blog }) => {
           <Image src={mainImage} alt={title} fill />
         </Link>
 
-        {/* <div className="px-4">
+        <div className="px-4">
           <h3 className="mb-3.5 mt-7.5 line-clamp-2 inline-block text-lg font-medium text-black duration-300 hover:text-primary dark:text-white dark:hover:text-primary xl:text-itemtitle2">
-             <Link href={{
-              pathname : "/blog/blog-details/",
-              query : {id : _id}
-             }}>
+            <Link href={{
+              pathname: "/blog/blog-details/",
+              query: { id: _id }
+            }}>
               {parse(`${title.slice(0, 40)}...` || '')}
             </Link>
           </h3>
-          <p className="line-clamp-3">{parse(metadata || '')}</p>
-        </div> */}
+          <div className="line-clamp-3">{parse(metadata || '')}</div>
+        </div>
       </motion.div>
     </>
   );
