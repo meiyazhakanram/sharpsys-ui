03fc0c9e481e48d685bae3f6d1c19f19ad8b5e3b
meiyazhakan <meiyazhakan86@gmail.com>
2024-11-19 09:15:02 +0530

    Update index.tsx

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { motion } from "framer-motion";
-import Image from "next/image";
 import { useState } from "react";
 import FAQItem from "./FAQItem";
 import faqData from "./faqData";
