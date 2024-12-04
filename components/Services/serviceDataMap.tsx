f97a92c9a8bed60180a3d6064bc502a320b2c4fe
Gowtham Selvm <gowthamgs92@gmail.com>
2024-12-04 23:50:25 +0530

    Counter and service image change

--- components/Services/serviceDataMap.tsx
+++ components/Services/serviceDataMap.tsx
@@ -1,19 +1,19 @@
 export const servicesDataMap = {
     "Enterprise Solution": [
-        { id: 101, order:"01", name: "Microsoft Dynamics 365 F&O", description:"Streamline finance and operations for global businesses.", img: "/images/services/servicestab/F&O.png" },
-        { id: 102, order:"02", name: "Microsoft Dynamics 365 Business Central", description:"A cloud-based solution for SMBs to manage and grow.", img: "/images/services/servicestab/Business Central.png" },
-        { id: 103, order:"03", name: "Microsoft Power Platform", description:"Empower data-driven decisions through automation and insights.", img: "/images/services/servicestab/Dynamics 365.png" },
+        { id: 101, order:"01", name: "Microsoft Dynamics 365 F&O", description:"Streamline finance and operations for global businesses.", img: "/images/services/servicestab/F&O.jpg" },
+        { id: 102, order:"02", name: "Microsoft Dynamics 365 Business Central", description:"A cloud-based solution for SMBs to manage and grow.", img: "/images/services/servicestab/Business Central.jpg" },
+        { id: 103, order:"03", name: "Microsoft Power Platform", description:"Empower data-driven decisions through automation and insights.", img: "/images/services/servicestab/Dynamics 365.jpg" },
     ],
     "CRM": [
-        { id: 201, order:"01", name: "Microsoft 365 Customer Engagement", description:"Enhance customer relationships with integrated solutions.", img: "/images/services/servicestab/Customer Engagement.jpg" },
-        { id: 202, order:"02", name: "Salesforce", description:"Drive sales growth and optimize CRM with Salesforce.", img: "/images/services/servicestab/Sales Force.jpg" },
+        { id: 201, order:"01", name: "Microsoft 365 Customer Engagement", description:"Enhance customer relationships with integrated solutions.", img: "/images/services/servicestab/Customer Engagement.jfif" },
+        { id: 202, order:"02", name: "Salesforce", description:"Drive sales growth and optimize CRM with Salesforce.", img: "/images/services/servicestab/Salesforce.png" },
     ],
     "Advanced Analytics": [
         { id: 301, order:"01", name: "Consulting", description: "Expert guidance to drive strategic business growth.", img: "/images/services/servicestab/Consulting.jpg" },
-        { id: 302, order:"02", name: "Data Analytics", description:"Unlock actionable insights with advanced analytics.", img: "/images/services/servicestab/Data Analytics.jpg" },
+        { id: 302, order:"02", name: "Data Analytics", description:"Unlock actionable insights with advanced analytics.", img: "/images/services/servicestab/Data Analytics.png" },
         { id: 303, order:"03", name: "ETL Mitigation", description:"Streamline data integration and minimize ETL issues.", img: "/images/services/servicestab/ETL Migration.jpg" },
-        { id: 304, order:"04", name: "Data Migration", description:"Ensure seamless, secure transfer of critical data.", img: "/images/services/servicestab/Data Migration.jpg" },
-        { id: 305, order:"05", name: "Data Warehousing", description:"Centralize and optimize data storage for easy access.", img: "/images/services/servicestab/Data Warehousing.jpg" },
+        { id: 304, order:"04", name: "Data Migration", description:"Ensure seamless, secure transfer of critical data.", img: "/images/services/servicestab/Data Migration.png" },
+        { id: 305, order:"05", name: "Data Warehousing", description:"Centralize and optimize data storage for easy access.", img: "/images/services/servicestab/Data Warehousing.png" },
         { id: 306, order:"06", name: "Machine Learning", description:"Leverage AI for predictive analytics and automation.", img: "/images/services/servicestab/Machine Learning.jpg" },
     ],
     "Business Intelligence": [
@@ -21,7 +21,7 @@ export const servicesDataMap = {
         { id: 402, order:"02", name: "Tableau", description:"Turn complex data into impactful visual stories.", img: "/images/services/servicestab/Tableau.jpg" },
     ],
     "Staff Augmentation": [
-        { id: 501, order:"01", name: "Industry Verticals", description:"", img: "/images/services/servicestab/Staff Augmentation.jpg" },
-        { id: 502, order:"02", name: "Resource Capabilities", description:"", img: "/images/services/servicestab/Staff Augmentation.jpg" },
+        { id: 501, order:"01", name: "Industry Verticals", description:"", img: "/images/services/servicestab/industry vertical.jfif" },
+        { id: 502, order:"02", name: "Resource Capabilities", description:"", img: "/images/services/servicestab/Resource capability.jfif" },
     ],
 };
