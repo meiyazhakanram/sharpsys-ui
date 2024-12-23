5ade0c220be327cbbb825bf0bb08feda24dc46b1
Gowtham Selvm <gowthamgs92@gmail.com>
2024-12-24 00:31:38 +0530

    Next config changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
