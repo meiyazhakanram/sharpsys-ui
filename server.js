12d0b735c8a66fdb0681e23b29e8397a6e626fd5
Gowtham Selvm <gowthamgs92@gmail.com>
2024-12-25 13:28:55 +0530

    server file update

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
- 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
- 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
- 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+const express = require('express');
+const next = require('next');
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
 
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(process.env.PORT || 3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
